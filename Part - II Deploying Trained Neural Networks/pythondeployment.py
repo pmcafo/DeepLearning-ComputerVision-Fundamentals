08f38119c6f3079b4e51d98961601203ba63f7cf
pmcafo <pmcafo@proton.me>
2023-12-26 08:10:35 +0000

    Remove images

--- Part - II Deploying Trained Neural Networks/pythondeployment.py
+++ Part - II Deploying Trained Neural Networks/pythondeployment.py
@@ -7,4 +7,5 @@ import matplotlib.pyplot as plt
 def deploy(img_path):
 
     caffe.set_mode_gpu()
-    MODEL_JOB_DIR = '/dli/data/digits/20180301-185638-
+    MODEL_JOB_DIR = '/dli/data/digits/20180301-185638-e918'
+    DATASET_JOB_DIR = '/
