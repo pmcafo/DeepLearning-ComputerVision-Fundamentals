5f45b82cfdca5c8fd974c54e2ba8ce55e721e7d4
pmcafo <pmcafo@proton.me>
2024-01-05 10:09:44 +0000

    Updating _data/apis.yaml via Laneworks CMS Publish

--- Part - II Deploying Trained Neural Networks/pythondeployment.py
+++ Part - II Deploying Trained Neural Networks/pythondeployment.py
@@ -8,4 +8,5 @@ def deploy(img_path):
 
     caffe.set_mode_gpu()
     MODEL_JOB_DIR = '/dli/data/digits/20180301-185638-e918'
-    DATASET_JOB_DIR = '/dli/data/digits/20180222
+    DATASET_JOB_DIR = '/dli/data/digits/20180222-165843-ada0'
+    ARCHITECTURE = MODEL_JOB_
